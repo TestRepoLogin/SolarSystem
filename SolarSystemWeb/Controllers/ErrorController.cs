51814bda8fa6847739b69a54d8efd2bbc1ea48cb
TestRepoLogin <purum65@mail.ru>
2015-09-02 23:14:33 +0300

    Добавил логирование исключений с помощью elmah и контроллер для отображения страниц ошибок

---
+++ SolarSystemWeb/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SolarSystemWeb.Controllers
+{
+    public class ErrorController : Controller
+    {
+        
+        public ActionResult NotFound(string aspxerrorpath)
+        {
+            return View((object)aspxerrorpath);
+        }
+
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }                       
+    }
+}
